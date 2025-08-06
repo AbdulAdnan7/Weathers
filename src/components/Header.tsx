eabb54e0ca18cd1b7e94656a428f37deea0a4e24
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-06 19:31:18 +0530

    Added theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
+import { useTheme } from '@/context/theme-provider';
+import { Moon, Sun } from 'lucide-react';
 import React from 'react'
 import { Link } from 'react-router'
 
 const Header = () => {
+  
+   const {theme, setTheme} = useTheme();
+   const isDark = theme === 'dark'
+
   return (
   <header className='sticky w-full z-50 top-0 border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
     <div className='container mx-auto flex h-16 items-center justify-between px-4'>
         <Link to={'/'}>
         <img 
         className='h-14'
-        src='/DarkMode.png' alt='darkMode' />
+        src={ isDark ? '/DarkMode.png' : '/LightModes.png'} alt='LogoMode' />
         </Link>
   
     <div>
         {/** For Search */}
         {/** For Toggle Theme */}
+        <div
+        className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? 'rotate-180' : 'rotate-0'}`}
+        onClick={() => setTheme(isDark ? "light" : "dark")}>{
+      isDark ? <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all' /> : <Moon className='h-6 w-6 text-rose-500 rotate-0 transition-all' /> 
+}
+</div>
     </div>
       </div>
   </header>
