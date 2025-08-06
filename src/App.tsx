e4e23bec7d00e48e97bcd9cd98b58adba976a1af
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-06 17:02:48 +0530

    Starting new Project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  const handleClick = () => {
+    console.log('Btn LCikec')
+  }
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button onClick={handleClick}>Click me</Button>
+    </div>
+  )
+}
+
+export default App
