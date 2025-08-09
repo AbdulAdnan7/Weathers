70ff24d34ad4614b2fc400c4e26a76aeb9f1d261
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-09 15:44:57 +0530

    New

--- src/Api/types.ts
+++ src/Api/types.ts
@@ -2,3 +2,10 @@ export interface Coordinates {
     lat: number,
     lon: number,
 }
+
+export interface WeatherConditions {
+    id: number,
+    main: string,
+    description: string,
+    icon: string,
+}
